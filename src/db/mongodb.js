e4e657a6f5f27d1f8f244f912604b1bbde0efa0c
navi_1998 <njot422@gmail.com>
2020-06-11 22:17:20 +0530

    hero deploy v2

--- src/db/mongodb.js
+++ src/db/mongodb.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/hungr", {useNewUrlParser: true,
+mongoose.connect("mongodb://taskapp:navjotsingh@cluster0-shard-00-00-4xokv.mongodb.net:27017,cluster0-shard-00-01-4xokv.mongodb.net:27017,cluster0-shard-00-02-4xokv.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority", {useNewUrlParser: true,
 useCreateIndex:true,
 useUnifiedTopology: true,
 useFindAndModify:false
